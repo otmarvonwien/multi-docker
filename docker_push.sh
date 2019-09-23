5791a469495e85d948b8d23aec708ca501843741
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-09-23 08:54:08 +0200

    modified .travis.yml

---
+++ docker_push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Log in to the docker CLI
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+# Take those images and push them to docker hub
+docker push otmarvonwien/multi-client
+docker push otmarvonwien/multi-nginx
+docker push otmarvonwien/multi-server
+docker push otmarvonwien/mutli-worker
