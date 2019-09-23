a5326029c281bcb0b033e93933a0405f3fe432ae
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-09-23 10:03:24 +0200

    modified .travis.yml and docker-push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker images
 # Log in to the docker CLI
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 # Take those images and push them to docker hub
