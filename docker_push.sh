fb285f57ce24ab7e7b5e0a064979aa315d398dbe
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-09-23 09:14:45 +0200

    modified .travis.yml and docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-docker build -t otmarvonwien/multi-client ./client
-docker build -t otmarvonwien/multi-nginx ./nginx
-docker build -t otmarvonwien/multi-server ./server
-docker build -t otmarvonwien/multi-worker ./worker
 # Log in to the docker CLI
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 # Take those images and push them to docker hub
