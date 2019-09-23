aff88b8965fb25f8b42f04a2174eec459e441658
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-09-23 14:22:09 +0200

    travis and dockerrun config

--- docker_push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Log in to the docker CLI
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-# Take those images and push them to docker hub
-docker push otmarvonwien/multi-client
-docker push otmarvonwien/multi-nginx
-docker push otmarvonwien/multi-server
-docker push otmarvonwien/multi-worker
